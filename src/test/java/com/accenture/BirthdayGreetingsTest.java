72e8ef545b5f60c58f53b3957124da9eb619f2fb
mina-taifour <mina.taifour@accenture.com>
2021-10-18 17:17:14 +0200

    Initialisation du test!

---
+++ src/test/java/com/accenture/BirthdayGreetingsTest.java
@@ -0,0 +1,13 @@
+package com.accenture;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BirthdayGreetingsTest {
+    //  date d'anniversaire de John est le 18/10
+    @Test
+    public void whenBirthdayOfJohnIsToday_thenMailBodyShouldbeAsExpected() {
+
+    }
+}
