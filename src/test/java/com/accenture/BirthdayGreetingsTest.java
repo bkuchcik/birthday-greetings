b62ec92ddaf8e46ae5be1f92672edf65621ddaa2
Gregoire <auriane.gregoire@accenture.com>
2021-10-19 11:11:36 +0200

    Test 2: date is not equal to birthdate

--- src/test/java/com/accenture/BirthdayGreetingsTest.java
+++ src/test/java/com/accenture/BirthdayGreetingsTest.java
@@ -10,12 +10,20 @@ import static org.junit.jupiter.api.Assertions.*;
 class BirthdayGreetingsTest {
     DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
     LocalDate localDate = LocalDate.now();
-    LocalDate date = localDate.of(2021,10,19);
-    Person john = new Person("John", "Jack", date, "john.jack@email.com");
+
     @Test
     public void whenBirthdayOfJohnIsToday_thenMailBodyShouldbeAsExpected() {
+        LocalDate date = localDate.of(2021, 10, 19);
+        Person john = new Person("John", "Jack", date, "john.jack@email.com");
+        assertEquals(dtf.format(localDate), dtf.format(john.getDateOfBirth()));
 
-       assertEquals(dtf.format(localDate),dtf.format(john.getDateOfBirth()));
+    }
+
+    @Test
+    public void whenBirthdayOfJohnIsToday_thenMailBodyNotShouldbeAsExpected() {
+        LocalDate date = localDate.of(2020, 10, 19);
+        Person mary = new Person("Mary", "Ann", date, "mary.ann@foobar.com");
+        assertFalse(dtf.format(localDate) == dtf.format(mary.getDateOfBirth()));
 
     }
 }
